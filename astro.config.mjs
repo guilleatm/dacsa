eef3b6e5a3e33cec557ae1ea06d5ddc49a077859
guilleatm <guillearnautm@gmail.com>
2026-02-25 22:43:28 +0100

    hrefs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://guilleatm.github.io',
-  base: '/dacsa',
+  base: '/dacsa/',
   vite: {
     plugins: [tailwindcss()],
   },
